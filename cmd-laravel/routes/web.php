c4a463a222dd73d0de259e22545c0c63f3ca1dfb
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2021-11-10 17:31:47 +0100

    prueba

--- cmd-laravel/routes/web.php
+++ cmd-laravel/routes/web.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+    //return "Hola mundo";
+});
+
+Route::get('/prueba', function () {
     //return view('welcome');
     return "Hola mundo";
 });
