60c336c895febcf3282ec78ffa6f49c9a8d802ac
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2022-02-28 11:31:44 +0100

    update ruta relativa

--- proyectoAPI/resources/views/layouts/base.blade.php
+++ proyectoAPI/resources/views/layouts/base.blade.php
@@ -11,7 +11,7 @@
         @include('layouts.footer')
 
     </div>
-    <script src="js/apirest.js"></script>
+    <script src="/LARAVEL/proyectoAPI/public/js/apirest.js"></script>
 </body>
 
 </html>
